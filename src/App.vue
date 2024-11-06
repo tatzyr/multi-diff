b415e2cce8eec0c2e371fecf48519a847cee1a96
Tatsuya Otsuka <tatzyr@gmail.com>
2024-11-06 23:39:34 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,106 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue';
+import DiffMatchPatch from 'diff-match-patch';
+
+const dmp = new DiffMatchPatch();
+
+const baseText = ref('foo, bar, baz, qux, quux, foobar');
+
+const fields = ref([
+  { value: 'foo, bar, baz, quux, foobar' },
+  { value: 'foo, quux, foobar, xyzzy' },
+]);
+
+const changeObjects = computed(() => {
+  return fields.value.map((field) => {
+    const diffs = dmp.diff_main(baseText.value, field.value);
+    dmp.diff_cleanupSemantic(diffs);
+    return diffs;
+  });
+});
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <h1>multi-diff</h1>
+
+    <div class="forms">
+      <div class="form-and-text">
+        <textarea v-model="baseText" class="text-input"></textarea>
+        <div class="result">{{ baseText }}</div>
+      </div>
+      <hr>
+
+      <transition-group name="list">
+        <template v-for="(field, i) in fields" :key="i">
+          <div class="form-and-text">
+            <textarea v-model="field.value" class="text-input"></textarea>
+            <div class="result">
+              <span v-for="(diff, j) in changeObjects[i]" :key="j" :class="diff[0] === 1 ? 'added' : diff[0] === -1 ? 'removed' : ''">
+                {{ diff[1] }}
+              </span>
+            </div>
+          </div>
+          <hr>
+        </template>
+      </transition-group>
+
+      <button @click="fields.push({ value: '' })">Add inputs</button>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.form-and-text {
+  display: flex;
+  gap: 1rem;
+  font-family: Hack, monospace;
+  text-align: left;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.text-input {
+  flex: 1;
+  min-height: 100px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.result {
+  flex: 1;
+  min-width: 0;
+  overflow-wrap: break-word;
+  white-space: pre-wrap;
+}
+
+.added {
+  font-weight: 600;
+  background: rgb(71, 142, 88);
+  border: 1px solid rgb(171, 242, 188);
+}
+
+.removed {
+  font-weight: 600;
+  background: rgb(142, 71, 71);
+  border: 1px solid rgb(242, 171, 171);
+}
+
+@media (prefers-color-scheme: light) {
+  .added {
+    background: rgb(171, 242, 188);
+    border: 1px solid rgb(71, 142, 88);
+  }
+
+  .removed {
+    background: rgb(242, 171, 171);
+    border: 1px solid rgb(142, 71, 71);
+  }
+}
+
+.list-enter-active, .list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from, .list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
